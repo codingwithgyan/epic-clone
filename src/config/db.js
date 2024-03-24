33463b267952c591c2f2b54f4698499c8c2786f1
Gyaneshwar Kumar <93430657+codingwithgyan@users.noreply.github.com>
2024-03-24 11:35:16 +0530

    updated mongo db url

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 module.exports = () => {
   return mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.imvhv.mongodb.net/epicgames`
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.6mssyct.mongodb.net/epicgames`
   );
 };
